30d4c7257737ec41bfd29a42a25ea7acd78cad7b
Erick Urbano <erick.urbano00@usc.edu.co>
2022-11-22 18:13:56 -0500

    Version de app nueva

---
+++ app/src/main/java/com/example/interfaceuser/MainActivity3.java
@@ -0,0 +1,22 @@
+package com.example.interfaceuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity3 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+    }
+}
